b8ac4ca4ab69392a25670123611e055003a0a522
Sonali Mehta <sonalikumari1604@gmail.com>
2026-04-19 22:20:14 +0530

    Time: 3 ms (28.28%) | Memory: 10 MB (37.04%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-    void powSet(int index, int n, vector<int>& nums, vector<int>& ds, vector<vector<int>>&ans){
-        if(index==n){
-            ans.push_back(ds);
-            return;
-        }
+    // void powSet(int index, int n, vector<int>& nums, vector<int>& ds, vector<vector<int>>&ans){
+    //     if(index==n){
+    //         ans.push_back(ds);
+    //         return;
+    //     }
 
-        ds.push_back(nums[index]);
-        powSet(index+1,n, nums ,ds, ans);
+    //     ds.push_back(nums[index]);
+    //     powSet(index+1,n, nums ,ds, ans);
 
-        ds.pop_back();
-        powSet(index+1,n,nums,ds,ans);
-    }
+    //     ds.pop_back();
+    //     powSet(index+1,n,nums,ds,ans);
+    // }
     vector<vector<int>> subsets(vector<int>& nums) {
     //recursive concept........
-    int n=nums.size();
-    vector<vector<int>>ans;
-    vector<int>ds;
+    // int n=nums.size();
+    // vector<vector<int>>ans;
+    // vector<int>ds;
 
-    powSet(0,n,nums,ds,ans);
-    return ans;
+    // powSet(0,n,nums,ds,ans);
+    // return ans;
 
-        //bit manupulation concept.....................
-    //    vector<vector<int>>ans;
-    //    int n = nums.size();
-    //    int iter = 1<<n; //2^n subsets
+    //bit manipulation
+     vector<vector<int>>ans;
+     int n= nums.size();
+     int iter = 1<<n;
 
-    //    for(int i=0;i<iter;i++){
-    //     vector<int>subset;
-    //     for(int j=0;j<n;j++){
-    //         if(i & (1<<j)){
-    //             subset.push_back(nums[j]);
-    //         }
-    //     }
-    //     ans.push_back(subset);
-    //    }
+     for(int i=0;i<iter;i++){
+        vector<int>subset;
+        for(int j=0;j<n;j++){
+            if(i & (1<<j)){
+                subset.push_back(nums[j]);
+            }
+        }
+        ans.push_back(subset);
+      } 
 
-    //    return ans;
+      return ans; 
     }
 };
