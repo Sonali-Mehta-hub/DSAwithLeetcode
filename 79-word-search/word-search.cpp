53c27c9b29c5a1f62d61862d87417dd94177ba12
Sonali Mehta <sonalikumari1604@gmail.com>
2026-04-27 08:11:27 +0530

    Time: 820 ms (24.82%) | Memory: 11.5 MB (39.22%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    bool helper(vector<vector<char>>&board, int i, int j, int m, int n, string word, int ind){
+    bool helper(vector<vector<char>>&board, int m, int n, int i, int j, int ind, string word){
+        
         if(ind == word.size()){
             return true;
         }
 
-       if(( i < 0 ||j < 0 || i >= m || j >= n ) || board[i][j]!= word[ind])return false;
+        if((i<0 ||j<0 ||i>=m || j>=n) || word[ind]!=board[i][j])return false;
 
-       char temp = board[i][j];
-       board[i][j]='#'; //mark visited
+        char temp = board[i][j];
+        board[i][j] = '*';//mark visited
 
-       bool found = helper(board, i+1, j, m, n, word, ind+1) || 
-                    helper(board, i-1, j, m, n, word, ind+1) ||
-                    helper(board, i, j+1, m, n, word, ind+1) ||
-                    helper(board, i, j-1, m, n, word, ind+1) ;
+        bool found = helper(board, m, n, i+1,j,ind+1,word)||
+                     helper(board, m, n, i-1,j,ind+1,word)||
+                     helper(board, m, n, i,j+1,ind+1,word)||
+                     helper(board, m, n, i,j-1,ind+1,word);
 
-        board[i][j] = temp; // backtrack
+        board[i][j]= temp;
 
         return found;
     }
@@ -23,15 +24,13 @@ public:
         int m = board.size();
         int n = board[0].size();
 
-        for(int i = 0; i<m ; i++){
-            for(int j = 0; j<n ; j++){
-                if(helper(board, i, j, m, n, word, 0)){
-                    return  true;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(helper(board,m,n,i,j,0,word)){
+                    return true;
                 }
-                
             }
         }
-
         return false;
     }
 };
