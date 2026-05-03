e3d20a013802b2cd5e524508bccf1c23a2b8b246
Sonali Mehta <sonalikumari1604@gmail.com>
2026-05-03 23:20:09 +0530

    Time: 17 ms (16.50%) | Memory: 18.7 MB (14.14%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    bool helper( const string &s, int n, unordered_set<string>&st,int start, vector<int>&dp){
+    bool helper(string s,int n, int start, unordered_set<string>&st, vector<int>&dp){
         if(start==n)return true;
-        if(dp[start]!=-1) return dp[start];
+        if(dp[start]!=-1)return dp[start];
 
-        for(int end=start;end<n;end++){
-            if(st.find(s.substr(start,end-start+1))!=st.end()){
-                if(helper(s,n,st,end+1,dp)){
+        for(int end = start;end<n;end++){
+            if(st.find(s.substr(start, end-start+1))!=st.end()){
+                if(helper(s,n,end+1,st,dp)){
                     return dp[start]=true;
                 }
             }
         }
-
         return dp[start]=false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
-
-        unordered_set<string> st(wordDict.begin(), wordDict.end());
+        unordered_set<string>st(wordDict.begin(),wordDict.end());
         vector<int>dp(n,-1);
-        return helper(s,n,st,0,dp);
-    
+         return helper(s,n,0,st,dp);
     }
 };
