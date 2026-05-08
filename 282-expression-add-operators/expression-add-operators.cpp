d919b876f5f2f704083cfa5346b3975059ff0523
Sonali Mehta <sonalikumari1604@gmail.com>
2026-05-08 10:03:49 +0530

    Time: 438 ms (26.86%) | Memory: 92.4 MB (77.82%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.cpp
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    void helper(string num,int target,int n, vector<string>&ans,int ind,long current_val,string expression,long last_op){
-        
+    void helper(string num, int target, int n, vector<string>&ans, int ind, long current_val, string expression, long last_op){
+
         if(ind==n){
             if(current_val==target){
                 ans.push_back(expression);
             }
             return;
         }
-        long current_number=0;
+
+        long current_number = 0;
+        string current_string="";
 
         for(int i=ind;i<n;i++){
-            if(i>ind && num[ind]=='0')break;
+            if(i>ind && num[ind]=='0'){
+                break;
+            }
 
             current_number = current_number*10+(num[i]-'0');
-            string current_string = num.substr(ind,i-ind+1);
+            current_string = num.substr(ind,i-ind+1);
 
             if(ind==0){
                 helper(num,target,n,ans,
@@ -26,34 +30,35 @@ public:
             else{
                 //addition
                 helper(num,target,n,ans,
-                        i+1,
-                        current_val+current_number,
-                        expression+"+"+current_string,
-                        current_number);
+                       i+1,
+                       current_val+current_number,
+                       expression+"+"+current_string,
+                       current_number);
 
                 //subtraction
                 helper(num,target,n,ans,
-                        i+1,
-                        current_val-current_number,
-                        expression+"-"+current_string,
-                        -current_number);
+                       i+1,
+                       current_val-current_number,
+                       expression+"-"+current_string,
+                       -current_number);
 
                 //multiplication
                 helper(num,target,n,ans,
-                        i+1,
-                        current_val-last_op+(last_op*current_number),
-                        expression+"*"+current_string,
-                        current_number*last_op);
+                       i+1,
+                       current_val-last_op+(last_op*current_number),
+                       expression+"*"+current_string,
+                       current_number*last_op);
             }
         }
+
+
     }
     vector<string> addOperators(string num, int target) {
         int n= num.size();
-
-        string expression ="";
+        string expression="";
         vector<string>ans;
 
-        helper(num,target,n,ans,0,0,expression,0);
+        helper(num, target, n,ans,0,0,expression, 0);
 
         return ans;
     }
