6fb65b8d67a1090ed5df64adec8a314d2b2102c0
Sonali Mehta <sonalikumari1604@gmail.com>
2026-05-11 22:57:44 +0530

    Time: 452 ms (13.96%) | Memory: 92.2 MB (80.58%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.cpp
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    void helper(string num, int target, int n, vector<string>&ans, int ind, long current_val, string expression, long last_op){
+    void helper(string num, int target,int n,vector<string>&ans,int ind, long current_value,string expression,long last_op){
 
         if(ind==n){
-            if(current_val==target){
+            if(current_value==target){
                 ans.push_back(expression);
             }
             return;
@@ -13,10 +13,8 @@ public:
         string current_string="";
 
         for(int i=ind;i<n;i++){
-            if(i>ind && num[ind]=='0'){
-                break;
-            }
-
+            if(i>ind && num[ind]=='0')break;
+            
             current_number = current_number*10+(num[i]-'0');
             current_string = num.substr(ind,i-ind+1);
 
@@ -31,35 +29,32 @@ public:
                 //addition
                 helper(num,target,n,ans,
                        i+1,
-                       current_val+current_number,
+                       current_number+current_value,
                        expression+"+"+current_string,
                        current_number);
 
                 //subtraction
                 helper(num,target,n,ans,
                        i+1,
-                       current_val-current_number,
+                       current_value-current_number,
                        expression+"-"+current_string,
                        -current_number);
 
                 //multiplication
                 helper(num,target,n,ans,
                        i+1,
-                       current_val-last_op+(last_op*current_number),
+                       current_value-last_op+(last_op*current_number),
                        expression+"*"+current_string,
                        current_number*last_op);
             }
         }
-
-
     }
     vector<string> addOperators(string num, int target) {
-        int n= num.size();
-        string expression="";
+        int n = num.size();
+        string expression ="";
         vector<string>ans;
 
-        helper(num, target, n,ans,0,0,expression, 0);
-
+        helper(num,target,n,ans,0,0,expression,0);
         return ans;
     }
 };
