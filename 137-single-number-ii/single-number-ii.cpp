9e027dc293a5ec52e20c45183545d2cd42924ce7
Sonali Mehta <sonalikumari1604@gmail.com>
2026-06-04 22:35:56 +0530

    Time: 3 ms (30.43%) | Memory: 13.9 MB (17.71%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        int ones = 0, twos = 0;
+        unordered_map<int,int>mp;
 
-        for(int i=0 ;i<n;i++){
-            ones = (ones^nums[i])&(~twos);
-            twos = (twos^nums[i])&(~ones);
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+
         }
-        return ones;
+        for(auto it:mp){
+            if(it.second==1){
+                return it.first;
+            }
+        }
+        return -1;
     }
 };
