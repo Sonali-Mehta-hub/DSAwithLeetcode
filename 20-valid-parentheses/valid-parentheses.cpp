b24a2f8b431671e988f4b68ac4c9cae407bd72cc
Sonali Mehta <sonalikumari1604@gmail.com>
2026-06-11 11:39:21 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (37.44%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+
+        for(auto ch:s){
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }
+            else {
+                if(st.empty())return false;
+                if(st.top()=='(' && ch==')' )st.pop();
+                else if(st.top()=='[' && ch==']' )st.pop();
+                else if(st.top()=='{' && ch=='}' )st.pop();
+
+                else return false;
+            }
+        }
+        if(st.empty())return true;
+        else return false;
+    }
+};
