79c2f2bf45e9113124d63390b6a26621831fdcaf
Sonali Mehta <sonalikumari1604@gmail.com>
2026-06-18 08:59:39 +0530

    Time: 1 ms (53.57%) | Memory: 12.1 MB (99.66%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            int j;
+            for(j=0;j<m;j++){
+                if(nums1[i]==nums2[j]){
+                    break;
+                }
+            }
+            int nge=-1;
+            for(int k=j+1;k<m;k++){
+                if(nums2[j]<nums2[k]){
+                    nge=nums2[k];
+                    break;
+                }
+               
+            }
+            ans.push_back(nge);
+          
+        }
+        return ans;
+    }
+};
