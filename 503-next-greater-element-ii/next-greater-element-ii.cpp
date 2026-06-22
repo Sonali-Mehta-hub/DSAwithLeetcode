61a80f3f31cb9943b0c84d0504f7fe5750358e5f
Sonali Mehta <sonalikumari1604@gmail.com>
2026-06-22 08:11:10 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (38.03%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
+        // int n = nums.size();
+        // vector<int>ans(n,-1);
+
+        // for(int i=0;i<n;i++){
+        //     for(int j=(i+1)%n;j!=i;j=(j+1)%n){
+        //         if(nums[i]<nums[j]){
+        //             ans[i]=nums[j];
+        //             break;
+        //         }
+
+        //     }
+        // }
+
+        // return ans;
+
         int n = nums.size();
-        vector<int>ans(n,-1);
+        stack<int>st;
+        vector<int>nge(n);
 
-        for(int i=0;i<n;i++){
-            for(int j=(i+1)%n;j!=i;j=(j+1)%n){
-                if(nums[i]<nums[j]){
-                    ans[i]=nums[j];
-                    break;
-                }
-
+        for(int i=2*n-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums[i%n]){
+                st.pop();
             }
+            if(i<n){
+                nge[i]=st.empty()?-1:st.top();
+            }
+            st.push(nums[i%n]);
         }
-
-        return ans;
-        
+        return nge;
     }
 };
