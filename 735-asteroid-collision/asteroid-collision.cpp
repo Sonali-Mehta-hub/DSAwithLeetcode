2b41b6e734bf1825502c3dde4652c8a67e1169ee
Sonali Mehta <sonalikumari1604@gmail.com>
2026-06-28 09:01:00 +0530

    Time: 3 ms (28.23%) | Memory: 21.6 MB (78.59%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -2,33 +2,26 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         int n = asteroids.size();
-        stack<int>st;
+        vector<int>st;
 
-        for(int i=0;i<n;i++){
+        for(int i =0;i<n;i++){
             if(asteroids[i]>0){
-                st.push(asteroids[i]);
+                st.push_back(asteroids[i]);
             }
             else{
-                while(!st.empty() && st.top()>0 && st.top()<abs(asteroids[i])){
-                    st.pop();
+                while(!st.empty() && st.back()>0 && st.back()<abs(asteroids[i])){
+                    st.pop_back();
                 }
-                if(!st.empty() && st.top()==abs(asteroids[i])){
-                    st.pop();
+
+                if(!st.empty() && st.back()==abs(asteroids[i])){
+                    st.pop_back();
                 }
-                else if(st.empty() || st.top()<0){
-                    st.push(asteroids[i]);
+                else if(st.empty() || st.back()<0){
+                    st.push_back(asteroids[i]);
                 }
             }
         }
 
-       vector<int>ans;
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
-        }
-
-        reverse(ans.begin(),ans.end());
-
-        return ans;
+        return st;
     }
 };
